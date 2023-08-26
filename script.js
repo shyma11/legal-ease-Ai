6e80dc8859fa23d0a4d6115541da45f675e9390c
shyma11 <muhabbatkhan40248@gmail.com>
2023-08-26 14:14:33 +0300

    html

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// window.addEventListener('scroll' , reveal);
+
+// function reveal(){
+//    let reveals = document.querySelectorAll('.reveal')
+
+//    for(let i = 0 ; i < reveals.length ; i++){
+
+//        let windowheight = innerHeight;
+//        let revealtop = reveals[i].getBoundingClientRect().top;
+//        let revealpoint = 150;
+
+//        if(revealtop < windowheight - revealpoint){
+//            reveals[i].classList.add('active');
+//        }
+//        else{
+          
+//            reveals[i].classList.remove('active');
+//        }
+//    }
+// }
